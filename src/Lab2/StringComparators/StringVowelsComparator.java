89f0d2615ad762e88ee6019c3882c3fdfe863346
Ejenaka <59808480+Ejenaka@users.noreply.github.com>
2021-02-02 20:09:47 +0200

    edit StringVowelsComparator for better readability

--- src/Lab2/StringComparators/StringVowelsComparator.java
+++ src/Lab2/StringComparators/StringVowelsComparator.java
@@ -6,26 +6,21 @@ public class StringVowelsComparator implements Comparator<String> {
 
     private final char[] vowels = {'A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u', 'Y', 'y'};
 
+    private int getVowelsCount(String str) {
+        int vowelsCount = 0;
+        for (int i = 0; i < str.length(); i++) {
+            for (char vowel : vowels) {
+                if (str.charAt(i) == vowel) {
+                    vowelsCount++;
+                }
+            }
+        }
+        return vowelsCount;
+    }
+
     @Override
     public int compare(String a, String b) {
-        int vowelsCountA = 0;
-        int vowelsCountB = 0;
-        for (int i = 0; i < a.length(); i++) {
-            for (char vowel : vowels) {
-                if (a.charAt(i) == vowel) {
-                    vowelsCountA++;
-                }
-            }
-        }
-        for (int i = 0; i < b.length(); i++) {
-            for (char vowel : vowels) {
-                if (b.charAt(i) == vowel) {
-                    vowelsCountB++;
-                }
-            }
-        }
-
-        return vowelsCountA - vowelsCountB;
+        return getVowelsCount(a) - getVowelsCount(b);
     }
 }
 
