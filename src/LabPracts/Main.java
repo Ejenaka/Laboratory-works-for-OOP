c4b63344914349da2a96119d4529e34892df7318
Ejenaka <59808480+Ejenaka@users.noreply.github.com>
2021-02-09 18:25:08 +0200

    add demonstration of prats functions

--- src/LabPracts/Main.java
+++ src/LabPracts/Main.java
@@ -2,6 +2,8 @@ package LabPracts;
 
 public class Main {
     public static void main(String[] args) {
-
+        PractsFunctions functions = new PractsFunctions();
+        int[] arr = {453,10000,676,432};
+        System.out.println(functions.getMaxDigitSumNumber(arr));
     }
 }
