f3b93d7d996ae46c2eb6a2b6d23241c2e225fe4d
Ejenaka <59808480+Ejenaka@users.noreply.github.com>
2021-02-16 01:46:57 +0200

    add User sign up function

--- src/Lab3/User.java
+++ src/Lab3/User.java
@@ -1,14 +1,27 @@
 package Lab3;
 
 public abstract class User {
-    private String name;
-    private String username;
+    protected String name;
+    protected String username;
     private String password;
-    protected boolean isSignedUp = false;
+    protected boolean isAuthorized = false;
 
     public User(String name, String username, String password) {
         this.name = name;
         this.username = username;
         this.password = password;
+    }
+
+    public void signUp(String username, String password) {
+        if (this.username.equals(username) && this.password.equals(password)) {
+            UserSystem.getInstance().addUser(this);
+        }
+    }
+
+    public void authorize(String username, String password) {
+        if (this.username.equals(username) && this.password.equals(password)) {
+            UserSystem.getInstance().authorizeUser(this);
+            isAuthorized = true;
+        }
     }
 }
