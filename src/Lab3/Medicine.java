aabb55d7c8670f03106c07f4fa48da91c8dce023
Ejenaka <59808480+Ejenaka@users.noreply.github.com>
2021-02-16 01:48:56 +0200

    add Edit Order options for editing order

--- src/Lab3/Medicine.java
+++ src/Lab3/Medicine.java
@@ -1,10 +1,10 @@
 package Lab3;
 
 public class Medicine {
-    private String name;
-    private int price;
-    private String producer;
-    private boolean isRequireDoctor;
+    protected String name;
+    protected int price;
+    protected String producer;
+    protected boolean isRequireDoctor;
 
     public Medicine(String name, int price, String producer, boolean isRequireDoctor) {
         this.name = name;
