a960e4610e5aec8fb57cc8a7b56bada25736139f
Ejenaka <59808480+Ejenaka@users.noreply.github.com>
2021-02-27 14:54:34 +0200

    fixing getMedicine method's in MedicineStore model

--- src/Lab3/MedicineStore.java
+++ src/Lab3/MedicineStore.java
@@ -1,6 +1,7 @@
 package Lab3;
 
 import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class MedicineStore {
@@ -14,29 +15,55 @@ public class MedicineStore {
         this.medicineStorage = new ArrayList<>(Arrays.asList(medicineStorage));
     }
 
-    public List<Medicine> getMedicineByName(String name) {
-        Object[] medicines = medicineStorage.stream().filter(medicine -> medicine.name.equals(name)).toArray();
-        if (medicines == null) {
-            throw new NullPointerException("Medicines not find");
+    public List<Medicine> getMedicineByName(String name) throws NoSuchElementException {
+        List<Medicine> searchResult = medicineStorage.stream()
+                .filter(medicine -> medicine.name.equals(name))
+                .collect(Collectors.toList());
+
+        if (!searchResult.isEmpty()) {
+            return searchResult;
+        } else {
+            throw new NoSuchElementException("Couldn't find these medicines");
         }
 
     }
 
-    public List<Medicine> getMedicineByPrice(int price) {
-        return medicineStorage.stream().filter(medicine -> medicine.price == price).findFirst().get();
+    public List<Medicine> getMedicineByPrice(int price) throws NoSuchElementException {
+        List<Medicine> searchResult = medicineStorage.stream()
+                .filter(medicine -> medicine.price == price)
+                .collect(Collectors.toList());
+
+        if (!searchResult.isEmpty()) {
+            return searchResult;
+        } else {
+            throw new NoSuchElementException("Couldn't find these medicines");
+        }
     }
 
-    public List<Medicine> getMedicineByProducer(String producer) {
-        return medicineStorage.stream().filter(medicine -> medicine.producer.equals(producer)).findFirst().get();
+    public List<Medicine> getMedicineByProducer(String producer) throws NoSuchElementException {
+        List<Medicine> searchResult = medicineStorage.stream()
+                .filter(medicine -> medicine.producer.equals(producer))
+                .collect(Collectors.toList());
+
+        if (!searchResult.isEmpty()) {
+            return searchResult;
+        } else {
+            throw new NoSuchElementException("Couldn't find these medicines");
+        }
     }
 
-    public Medicine getMedicine(String name, int price, String producer) {
-        return medicineStorage.stream()
+    public Medicine getMedicine(String name, int price, String producer) throws NoSuchElementException {
+        Optional<Medicine> optionalMedicine = medicineStorage.stream()
                 .filter(medicine -> medicine.name.equals(name)
                         && medicine.price == price
                         && medicine.producer.equals(producer))
-                .findFirst()
-                .get();
+                .findFirst();
+
+        if (optionalMedicine.isPresent()) {
+            return optionalMedicine.get();
+        } else {
+            throw new NoSuchElementException("Couldn't find these medicines");
+        }
     }
 
 }
