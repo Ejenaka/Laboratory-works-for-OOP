46f6a856cda86c881a46f567ba54df3d2def5850
Ejenaka <59808480+Ejenaka@users.noreply.github.com>
2021-02-27 19:01:36 +0200

    add interface MedicineGetable

---
+++ src/Lab3/MedicineGetable.java
@@ -0,0 +1,10 @@
+package Lab3;
+
+import java.util.List;
+
+public interface MedicineGetable {
+    List<Medicine> getMedicineByName(String name);
+    List<Medicine> getMedicineByPrice(int price);
+    List<Medicine> getMedicineByProducer(String producer);
+    Medicine getMedicine(String name, int price, String producer);
+}
