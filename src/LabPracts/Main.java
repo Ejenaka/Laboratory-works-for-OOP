74513b4d4187aeb29b5040d5d97e05a087b6e6f3
Ejenaka <59808480+Ejenaka@users.noreply.github.com>
2021-04-06 13:01:10 +0300

    add functions for practs

--- src/LabPracts/Main.java
+++ src/LabPracts/Main.java
@@ -1,9 +1,12 @@
 package LabPracts;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         PractsFunctions functions = new PractsFunctions();
-        int[] arr = {453,10000,676,432};
-        System.out.println(functions.getMaxDigitSumNumber(arr));
+        int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        Integer[][] result = functions.getPairsBySum(nums, 9);
+        System.out.println(Arrays.deepToString(result));
     }
 }
--- src/LabPracts/PractsFunctions.java
+++ src/LabPracts/PractsFunctions.java
@@ -1,5 +1,8 @@
 package LabPracts;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class PractsFunctions {
     public int getMaxDigitSumNumber(int[] array) {
         int maxSum = 0;
@@ -18,6 +21,22 @@ public class PractsFunctions {
             return num;
         }
         return num % 10 + getDigitSum(num / 10);
+    }
+
+    // 4 LAB
+    public Integer[][] getPairsBySum(int[] nums, int sum) {
+        ArrayList<Integer[]> pairs = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i+1; j < nums.length; j++ ) {
+                if (nums[i] + nums[j] == sum) {
+                    pairs.add(new Integer[] {nums[i], nums[j]});
+                }
+            }
+        }
+        if (pairs.isEmpty()) {
+            System.out.println("No pairs are given");
+        }
+        return pairs.toArray(new Integer[0][0]);
     }
 
 }
